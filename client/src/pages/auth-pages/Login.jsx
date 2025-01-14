c36c21391510cf899c5d47b535e56104e4b62819
S-Alif <alif160038@gmail.com>
2025-01-14 22:31:54 +0600

    client - done - login page

--- client/src/pages/auth-pages/Login.jsx
+++ client/src/pages/auth-pages/Login.jsx
@@ -2,51 +2,60 @@ import AuthPagesLayout from "@/components/AuthPagesLayout"
 import ManualForm from "@/components/manual-form/ManualForm"
 import ManualInput from "@/components/manual-form/ManualInput"
 import { buttonVariants } from "@/components/ui/button"
+import { useRef } from "react"
 import { NavLink } from "react-router"
 
 const Login = () => {
 
-  const formSubmit = (e) => {
-    console.log(e)
-  } 
+    const formRef = useRef()
 
-  return (
-      <AuthPagesLayout
-        pageImage="https://images.unsplash.com/photo-1617039346892-d72191157374?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        id="login-page"
-        pageTitle="Login"
-        belowTitleText="Please sign-in to your account"
-      >
-        <ManualForm
-            formId="login-form"
-            buttonText="Login"
-            buttonSize="lg"
-            onSubmit={formSubmit}
+    const formSubmit = (e) => {
+        console.log(e)
+        formRef.current.resetForm()
+    }
+
+    return (
+        <AuthPagesLayout
+            pageImage="https://images.unsplash.com/photo-1617039346892-d72191157374?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            id="login-page"
+            pageTitle="Login"
+            belowTitleText="Please sign-in to your account"
         >
-            <ManualInput
-                field="input"
-                fieldType="email"
-                fieldLabel="Email address"
-                name="email"
-                placeholder="Enter email"
-            />
+            <ManualForm
+                formId="login-form"
+                buttonText="Login"
+                buttonSize="lg"
+                onSubmit={formSubmit}
+                ref={formRef}
+                defaultValues={{
+                    email: "",
+                    pass: ""
+                }}
+            >
+                <ManualInput
+                    field="input"
+                    fieldType="email"
+                    fieldLabel="Email address"
+                    name="email"
+                    placeholder="Enter email"
+                />
 
-            <ManualInput
-                field="input"
-                fieldType="password"
-                fieldLabel="Your Password"
-                name="pass"
-                placeholder="Enter pass"
-            />
+                <ManualInput
+                    field="input"
+                    fieldType="password"
+                    fieldLabel="Your Password"
+                    name="pass"
+                    placeholder="Enter password"
+                />
 
-        </ManualForm>
+            </ManualForm>
 
-        <p className="!text-[17px] pt-10">
-            Don't have an account ? 
-            <NavLink className={`!pl-2 text-[17px] ${buttonVariants({variant: "link"})}`}>Sign in</NavLink>
-        </p>
-      </AuthPagesLayout>
-  )
+            <p className="!text-[17px] pt-10">
+                Don't have an account ?
+                <NavLink className={`!pl-2 text-[17px] ${buttonVariants({ variant: "link" })}`}>Sign in</NavLink>
+            </p>
+        </AuthPagesLayout>
+    )
 }
 
 export default Login
