4a98a46bfa2a88ffeb4064fba18b54906ad5b833
S-Alif <alif160038@gmail.com>
2025-01-18 21:00:36 +0600

    client - add - partial admin routes

--- client/src/constants/apiConstants.js
+++ client/src/constants/apiConstants.js
@@ -18,3 +18,12 @@ export const publicRoutes = {
     verifyOtp: `${baseUrl}/api/v1/${publicEndpoint}/verify`,
     updatePass: `${baseUrl}/api/v1/${publicEndpoint}/update-pass`,
 }
+
+export const adminRoutes = {
+    // category
+    category: `${baseUrl}/api/v1/${adminEndpoint}/category`,
+    // colors
+    colors: `${baseUrl}/api/v1/${adminEndpoint}/colors`,
+    // sizes
+    sizes: `${baseUrl}/api/v1/${adminEndpoint}/size`,
+}
